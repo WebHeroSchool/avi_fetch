1f921dc8c74cd3fff98ae793379e984151a64953
Anya Vigolajnen <vigolajnen@gmail.com>
2020-12-24 17:37:55 +0300

    правки

--- script2.js
+++ script2.js
@@ -1,44 +1,60 @@
 window.setTimeout(function () {
   document.body.classList.add('loaded');
   let body = document.body;
-  let url = 'https://api.github.com/users/vigolajnen';
+  let newUser;
+  let url = window.location.toString();
+
+  const getUserUrl= (url) => {
+    let getUrl = url.split('=');
+    let userName = getUrl[1]; //
+    if (userName == undefined) {
+      userName = 'lucas';
+    }
+    console.log(userName);
+    return userName;
+  }
+
+  let getUrl = getUserUrl(url);
+
   const date = new Date();
+
   const getDate = new Promise((resolve, reject) => {
     setTimeout(() => date ? resolve(date) : reject('Ошибка'), 200)
-  })
-  const getUrl = new Promise((resolve, reject) => {
-    setTimeout(() => url ? resolve(url) : reject('URL не найден!'), 200)
-  })
+  });
 
-   Promise.all([getUrl, getDate])
-      .then(([url, date]) => fetch(url))
-      .then(res => res.json())
-      .then(json => {
-        let avatar = document.createElement('img'),
-          userName = document.createElement('h2'),
-          userDesc = document.createElement('p'),
-          userLink = document.createElement('a');
-          userDate = document.createElement('p');
 
-        avatar.src = json.avatar_url;
-        userLink.href = json.html_url;
-        userLink.innerText = 'my github';
+  const renderUserCard = function (obj) {
+    let avatar = document.createElement('img'),
+        userName = document.createElement('h2'),
+        userDesc = document.createElement('p'),
+        userLink = document.createElement('a');
+        userDate = document.createElement('p');
 
-        if (json.login != null) {
-          userName.innerHTML = json.login;
-          } else {
-            userName.innerHTML = 'Информация о пользователе недоступна';
-        }
-        
-        if (json.bio != null) {
-          userDesc.innerHTML = json.bio;
-          } else {
-            userDesc.innerHTML = 'Информация о пользователе недоступна';
-        }
+    avatar.src = obj.avatar_url;
+    userLink.href = obj.html_url;
+    userLink.innerText = 'my github';
 
-        userDate.innerHTML = date;
-        body.prepend(avatar, userName, userDesc, userLink, userDate);
-    })
+    if (obj.login != null) {
+      userName.innerHTML = obj.login;
+      } else {
+        userName.innerHTML = 'Информация о пользователе недоступна';
+    }
+    
+    if (obj.bio != null) {
+      userDesc.innerHTML = obj.bio;
+      } else {
+        userDesc.innerHTML = 'Информация о пользователе недоступна';
+    }
 
+    userDate.innerHTML = date;
+    body.prepend(avatar, userName, userDesc, userLink, userDate);
+  }
+
+  fetch(getUrl)
+  Promise.all([getDate])
+    .then(() => fetch(getUrl))
+    .then(res => res.json())
+    .then(obj => newUser = Object.assign({}, obj))
+    .then(newUser => renderUserCard(newUser))
     .catch(err => console.log('Информация о пользователе недоступна'));
  }, 3000);
